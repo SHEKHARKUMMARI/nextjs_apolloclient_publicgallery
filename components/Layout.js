89dcde8e757f5c990618ecec96de1a2df8496ef0
SHEKHARKUMMARI <kummarishekhar9@gmail.com>
2021-10-06 12:44:35 +0530

    changed on 6 oct

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 const layout=({children})=>{
     return <>
     <Head>
+    <title>PUBLIC GALLERY</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
     <link rel="stylesheet" href="/style.css" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
